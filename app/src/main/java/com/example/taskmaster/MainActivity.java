5b8e749529317282c5860d5646910a59e072a7ee
Ali Al-hjouj <alisalamehhjouj@gmail.com>
2021-11-15 15:25:05 +0200

    solve lab 27

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.taskmaster;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
-import com.google.android.material.textfield.TextInputEditText;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +25,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView imageView =findViewById(R.id.imageView2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent setting= new Intent(MainActivity.this,SettingsPage.class);
+                startActivity(setting);
+            }
+        });
+
 
         Button AddTask = findViewById(R.id.AddTask);
         AddTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"I'm click",Toast.LENGTH_LONG).show();
                 Intent AddTask= new Intent(MainActivity.this,AddTask.class);
-
                 startActivity(AddTask);
             }
         });
@@ -33,10 +47,55 @@ public class MainActivity extends AppCompatActivity {
         AllTasks.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"I'm click",Toast.LENGTH_LONG).show();
                 Intent AllTasks= new Intent(MainActivity.this,AllTaskes.class);
                 startActivity(AllTasks);
             }
         });
+        Button sport = findViewById(R.id.sport);
+        sport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sport= new Intent(MainActivity.this,TaskDetail.class);
+                sport.putExtra("title","sport");
+                startActivity(sport);
+            }
+        });
+        Button read = findViewById(R.id.read);
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent read= new Intent(MainActivity.this,TaskDetail.class);
+                read.putExtra("title","read");
+                startActivity(read);
+            }
+        });
+        Button sleep = findViewById(R.id.sleep);
+        sleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sleep= new Intent(MainActivity.this,TaskDetail.class);
+                sleep.putExtra("title","sleep");
+                startActivity(sleep);
+            }
+        });
+//        Button setting = findViewById(R.id.setting);
+//        setting.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent setting= new Intent(MainActivity.this,SettingsPage.class);
+//                startActivity(setting);
+//            }
+//        });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        String name = sharedPreferences.getString("userName","username");
+        TextView textView = findViewById(R.id.name);
+        textView.setText(name+"'s " +"Tasks");
     }
 }
--- app/src/main/java/com/example/taskmaster/SettingsPage.java
+++ app/src/main/java/com/example/taskmaster/SettingsPage.java
@@ -4,35 +4,48 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
-public class AddTask extends AppCompatActivity {
+public class SettingsPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_task);
+        setContentView(R.layout.activity_settings_page);
 
         ActionBar actionBar = getSupportActionBar();
 
         // showing the back button in action bar
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        Button button =findViewById(R.id.fromAddTask);
+        Button button = findViewById(R.id.save);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(AddTask.this,"submitted!",Toast.LENGTH_LONG).show();
+
+                TextInputEditText textInputEditText = findViewById(R.id.user);
+                String userName = textInputEditText.getText().toString();
+
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(SettingsPage.this);
+
+                SharedPreferences.Editor share = sharedPreferences.edit();
+                share.putString("userName",userName);
+                share.apply();
+
+                Toast.makeText(SettingsPage.this,userName,Toast.LENGTH_LONG).show();
+                Intent AddTask= new Intent(SettingsPage.this,MainActivity.class);
+                startActivity(AddTask);
             }
         });
-
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
