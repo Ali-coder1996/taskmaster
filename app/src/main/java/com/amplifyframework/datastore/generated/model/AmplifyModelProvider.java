a3ef5c1c71e4c7c6c3e0e0449fd5fcfaf602f17f
Ali Al-hjouj <alisalamehhjouj@gmail.com>
2021-11-27 17:39:12 +0200

    aolve lab 33

--- app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
@@ -13,7 +13,7 @@ import java.util.Set;
  */
 
 public final class AmplifyModelProvider implements ModelProvider {
-  private static final String AMPLIFY_MODEL_VERSION = "77a656b5650711c9662195d7032bbc82";
+  private static final String AMPLIFY_MODEL_VERSION = "692d99db7782a52614ca086035954c20";
   private static AmplifyModelProvider amplifyGeneratedModelInstance;
   private AmplifyModelProvider() {
     
